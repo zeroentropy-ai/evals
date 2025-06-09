1afa3f81b454657e2201099bce47b5f8d99a82a6
Advaith Avadhanam <advaith5@mit.edu>
2025-06-09 16:34:00 -0700

    remove unused variables

--- evals/ndcg/zeroentropy_eval.py
+++ evals/ndcg/zeroentropy_eval.py
@@ -2,8 +2,6 @@ import aiohttp
 from tqdm.asyncio import tqdm
 from evals.utils import ROOT
 # pyright: reportUnknownMemberType=false
-DATASET_PATH = f"{ROOT}/data/datasets/bioasq/ai_scores.json"
-USE_CLIENT = True
 async def rerank_batch(query_documents: list[tuple[str, str]], client) -> list[float]:
     url = "https://npip99--testing-ze-rerank-1-model-endpoint.modal.run"
     headers = {
@@ -13,10 +11,9 @@ async def rerank_batch(query_documents: list[tuple[str, str]], client) -> list[f
     payload = {
         "query_documents": query_documents,
     }
-    if USE_CLIENT:
-        assert client is not None
-        async with client.post(url, headers=headers, json=payload) as response:
-            result = await response.json()
-            scores = [float(score) for score in result["scores"]]
+    assert client is not None
+    async with client.post(url, headers=headers, json=payload) as response:
+        result = await response.json()
+        scores = [float(score) for score in result["scores"]]
     assert len(scores) == len(query_documents)
     return scores
